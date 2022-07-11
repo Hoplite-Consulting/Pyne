2c732edba5613282f24f68ed59c08aee2aa3a40e
oliv10 <revi99@me.com>
2022-07-11 18:31:20 +0000

    Add Utils

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementTree import Element
+
+def getReportItems(elmnt: Element, elmntList: list) -> list[dict]:
+    retList = []
+    for item in elmnt.findall("./ReportItem"):
+        appDict = elmnt.attrib | item.attrib
+        for var in elmntList:
+            try:
+                appDict[var] = item.find(f"./{var}").text[:32600]
+            except:
+                pass
+        retList.append(appDict)
+    return retList
