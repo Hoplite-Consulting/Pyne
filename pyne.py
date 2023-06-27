4f08791a7b211679d81d06d53c891e70dfa1be64
Oliver Scotten <revi99@me.com>
2023-06-27 22:30:41 +0000

    Version 1.2.4-Custom This version is used in the .app beta version.

--- pyne.py
+++ pyne.py
@@ -6,7 +6,6 @@ import argparse
 import xml.etree.ElementTree as ET
 from alive_progress import alive_it
 import time
-from os.path import exists
 import pyfiglet
 
 # Get Default REPORT.conf
@@ -20,29 +19,29 @@ def main(args):
     print(TITLE)
 
     reports = []
-    for file in args.nessusFiles:
-        try:
-            scan = ET.parse(file)
-        except:
-            print("Unable to open file ", file)
-            continue
-        bar = alive_it(scan.getroot().findall('./Report/ReportHost'), title="Reading Reports...")
-        for reportHost in bar:
-            if args.SlowMode:
-                time.sleep(.01)
-            repHost = utils.getHostItems(reportHost, HOST)
-            repItems = utils.getReportItems(reportHost, VARS)
-            for report in repItems:
-                report["filename"] = file.split("/")[-1] # Add Filename to Report
-                if args.CATEGORY:
-                    report["category"] = utils.getCategory(report['pluginName'], report['solution'], report['severity']) # Add Category to Report
-                if args.UID:
-                    try:
-                        report["uid"] = report["pluginID"] + "-" + report["port"] + "-" + repHost["host-fqdn"]
-                    except KeyError:
-                        report["uid"] = report["pluginID"] + "-" + report["port"] + "-" + repHost["host-ip"]
-                        print("[WARNING] No host-fqdn found for", repHost["host-ip"], "Using host-ip, this may not be consistent if comparing multiple scans.")
-                reports.append(report | repHost)
+    # for file in args.nessusFiles:
+    try:
+        scan = ET.parse(args.nessusFile)
+    except:
+        print("Unable to open file ", args.nessusFile)
+        exit()
+    bar = alive_it(scan.getroot().findall('./Report/ReportHost'), title="Reading Reports...")
+    for reportHost in bar:
+        if args.SlowMode:
+            time.sleep(.01)
+        repHost = utils.getHostItems(reportHost, HOST)
+        repItems = utils.getReportItems(reportHost, VARS)
+        for report in repItems:
+            report["filename"] = args.nessusFile.split("/")[-1] # Add Filename to Report
+            if args.CATEGORY:
+                report["category"] = utils.getCategory(report['pluginName'], report['solution'], report['severity']) # Add Category to Report
+            if args.UID:
+                try:
+                    report["uid"] = report["pluginID"] + "-" + report["port"] + "-" + repHost["host-fqdn"]
+                except KeyError:
+                    report["uid"] = report["pluginID"] + "-" + report["port"] + "-" + repHost["host-ip"]
+                    print("[WARNING] No host-fqdn found for", repHost["host-ip"], "Using host-ip, this may not be consistent if comparing multiple scans.")
+            reports.append(report | repHost)
     
     # Get Default SORT.conf
     keys = utils.readConfig("config/SORT.conf")
@@ -56,26 +55,14 @@ def main(args):
         keys.sort()
 
     # Save to File
-    if args.writeFile:
-        if exists(args.writeFile):
-            while True:
-                if args.force:
-                    break
-                i = input("Overwrite (y/n): ")
-                if i.lower() == "y":
-                    pass
-                    break
-                elif i.lower() == "n":
-                    print("Exiting")
-                    exit()
-        with open(args.writeFile, "w") as f:
-            writer = DictWriter(f, keys)
-            writer.writeheader()
-            bar = alive_it(reports, title="Writing to file...")
-            for rep in bar:
-                if args.SlowMode:
-                    time.sleep(0.01)
-                writer.writerow(rep)
+    with open(f"{args.nessusFile}.csv", "w") as f:
+        writer = DictWriter(f, keys)
+        writer.writeheader()
+        bar = alive_it(reports, title="Writing to file...")
+        for rep in bar:
+            if args.SlowMode:
+                time.sleep(0.01)
+            writer.writerow(rep)
 
 if __name__ == "__main__":
 
@@ -84,8 +71,8 @@ if __name__ == "__main__":
     TITLE = pyfiglet.figlet_format(NAME, font="stop") + f"\n{NAME} {__version__}\n"
 
     parser = argparse.ArgumentParser(description=f"{TITLE}", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('nessusFiles', type=str, nargs='+', help='nessus file')
-    parser.add_argument('writeFile', help='path to write file')
+    parser.add_argument('nessusFile', type=str, help='nessus file')
+    # parser.add_argument('-w', '--writeFile', help='path to write file')
     parser.add_argument('-s', '--sort', action='store_true', help='sort keys alphabetically')
     parser.add_argument('-f', '--force', action='store_true', help='force overwrite of write file')
     parser.add_argument('-C', '--CATEGORY', action='store_true', help='add category to each finding')
