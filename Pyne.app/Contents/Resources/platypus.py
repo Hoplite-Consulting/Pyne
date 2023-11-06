b3a7e2b760ad3ef01b3dccf0517ebb8b7dd3afc7
Oliver Scotten <revi99@me.com>
2023-11-06 21:30:25 +0000

    Remove Pyne.app

--- Pyne.app/Contents/Resources/platypus.py
+++
@@ -1,5 +0,0 @@
-def platypusNotification(title: str, message: str):
-    print(f"NOTIFICATION:{title}|{message}\n")
-
-def platypusAlert(title: str, message: str):
-    print(f"ALERT:{title}|{message}\n")
