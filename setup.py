d3030cad96cb0c1ba9237c493768457512f549fe
Oliver Scotten <revi99@me.com>
2023-11-06 21:01:31 +0000

    Update Setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author_email="oliver@hopliteconsulting.com",
     packages=find_packages(),
     install_requires=[
-        "alive_progress>=3.1.4",
-        "pyfiglet>=1.0.2"
+        "alive_progress==3.1.4",
+        "pyfiglet==1.0.2"
     ],
     entry_points={
         "console_scripts": [
